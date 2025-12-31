41a06b6e4f375a713308bbf004c7c1bdf1f42eef
shaharebhavesh <shaharebhavesh@gmail.com>
2025-12-31 16:21:44 +0530

    change in mail for controller reply

--- src/main/java/com/tarana/soyafoods/service/GmailServiceUtil.java
+++ src/main/java/com/tarana/soyafoods/service/GmailServiceUtil.java
@@ -1,7 +1,8 @@
 package com.tarana.soyafoods.service;
 
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
@@ -11,19 +12,26 @@ public class GmailServiceUtil {
     private static final String APPLICATION_NAME = "Tarana Soya Foods";
     private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
+    private static final String TOKEN_SERVER_URL =
+            "https://oauth2.googleapis.com/token";
+
     public static Gmail getGmailService() throws Exception {
 
-        Credential credential = new GoogleCredential.Builder()
+        Credential credential = new Credential.Builder(
+                BearerToken.authorizationHeaderAccessMethod()
+        )
                 .setTransport(GoogleNetHttpTransport.newTrustedTransport())
                 .setJsonFactory(JSON_FACTORY)
-                .setClientSecrets(
-                        System.getenv("GMAIL_CLIENT_ID"),
-                        System.getenv("GMAIL_CLIENT_SECRET")
+                .setTokenServerEncodedUrl(TOKEN_SERVER_URL)
+                .setClientAuthentication(
+                        new ClientParametersAuthentication(
+                                System.getenv("GMAIL_CLIENT_ID"),
+                                System.getenv("GMAIL_CLIENT_SECRET")
+                        )
                 )
-                .build()
-                .setRefreshToken(System.getenv("GMAIL_REFRESH_TOKEN"));
+                .build();
 
-        credential.refreshToken(); // headless auth
+        credential.setRefreshToken(System.getenv("GMAIL_REFRESH_TOKEN"));
 
         return new Gmail.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
